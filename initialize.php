23211bc532aa099afec941f0c71ab7ceab199a82
Danilo Alvarado <daniloalvarado2002@gmail.com>
2025-07-17 20:10:35 -0500

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -13,7 +13,8 @@
 
 // PARA EL RAILWAY DATABASE CONEXIÓN EN LA NUBE
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/Sistema de Facturación 2/');
+// if(!defined('base_url')) define('base_url','http://localhost/Sistema de Facturación 2/');
+if(!defined('base_url')) define('base_url','https://sistema-de-facturacion-8yk6.onrender.com/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 define('DB_SERVER', 'switchyard.proxy.rlwy.net');
