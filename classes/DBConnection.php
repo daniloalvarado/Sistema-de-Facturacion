396ee0c130d3c5e03ffe96286acdc063529641ad
Danilo Alvarado <daniloalvarado2002@gmail.com>
2025-07-17 19:11:34 -0500

    Update DBConnection.php

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -1,30 +1,70 @@
 <?php
+// PARA CONEXION LOCALHOST (XAMPP)
+// if (!defined('DB_SERVER')) {
+//     require_once("../initialize.php");
+// }
+// class DBConnection
+// {
+//     private $host = DB_SERVER;
+//     private $username = DB_USERNAME;
+//     private $password = DB_PASSWORD;
+//     private $database = DB_NAME;
+
+//     public $conn;
+
+//     public function __construct()
+//     {
+//         if (!isset($this->conn)) {
+
+//             $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+//             if (!$this->conn) {
+//                 echo 'Cannot connect to database server';
+//                 exit;
+//             }
+//         }
+//     }
+//     public function __destruct()
+//     {
+//         $this->conn->close();
+//     }
+// }
+// FINAL PARA CONEXION LOCALHOST (XAMPP)
+
+
+// PARA EL RAILWAY DATABASE CONEXIÓN EN LA NUBE
 if (!defined('DB_SERVER')) {
     require_once("../initialize.php");
 }
+
 class DBConnection
 {
     private $host = DB_SERVER;
     private $username = DB_USERNAME;
     private $password = DB_PASSWORD;
     private $database = DB_NAME;
+    private $port = DB_PORT; // 👈 AÑADIDO
 
     public $conn;
 
     public function __construct()
     {
         if (!isset($this->conn)) {
+            // 👇 AÑADIDO EL PUERTO EN LA CONEXIÓN
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database, $this->port);
 
-            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
-
-            if (!$this->conn) {
-                echo 'Cannot connect to database server';
+            if ($this->conn->connect_error) {
+                echo 'Cannot connect to database server: ' . $this->conn->connect_error;
                 exit;
             }
         }
     }
+
     public function __destruct()
     {
-        $this->conn->close();
+        if ($this->conn) {
+            $this->conn->close();
+        }
     }
 }
+// FINAL PARA EL RAILWAY DATABASE CONEXIÓN EN LA NUBE
