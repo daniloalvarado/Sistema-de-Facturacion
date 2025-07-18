4aa13f7649f502644bc70041f8a3a3cafb6d35e8
Danilo Alvarado <daniloalvarado2002@gmail.com>
2025-07-18 17:04:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  lo envia automáticamente a http://localhost/tu-sistema/admin/. -->
 <?php 
 ob_start();
-header("Location: /admin/?page=home");
+header("Location: admin/?page=home");
 ob_end_clean();
 exit;
 ?>
