73af7bae9f563d567d7b1fd86fdce412606e4548
Danilo Alvarado <daniloalvarado2002@gmail.com>
2025-10-18 21:36:11 -0500

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -12,16 +12,36 @@
 
 
 // PARA EL RAILWAY DATABASE CONEXIÓN EN LA NUBE
+// $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+// // if(!defined('base_url')) define('base_url','http://localhost/Sistema de Facturación 2/');
+// if(!defined('base_url')) define('base_url','https://sistema-de-facturacion-8yk6.onrender.com/');
+// if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+// if(!defined('dev_data')) define('dev_data',$dev_data);
+// define('DB_SERVER', 'switchyard.proxy.rlwy.net');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', 'CSkndWRkJgACSBtpxwSMYXOkEVEmBWkw');
+// define('DB_NAME', 'invoice_db');
+// define('DB_PORT', 30688); 
+// FINAL PARA EL RAILWAY DATABASE CONEXIÓN EN LA NUBE
+
+
+// Datos del desarrollador (sin cambios)
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-// if(!defined('base_url')) define('base_url','http://localhost/Sistema de Facturación 2/');
+
+// URL base (sin cambios, apunta a Render)
 if(!defined('base_url')) define('base_url','https://sistema-de-facturacion-8yk6.onrender.com/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
-define('DB_SERVER', 'switchyard.proxy.rlwy.net');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'CSkndWRkJgACSBtpxwSMYXOkEVEmBWkw');
-define('DB_NAME', 'invoice_db');
-define('DB_PORT', 30688); 
-// FINAL PARA EL RAILWAY DATABASE CONEXIÓN EN LA NUBE
+
+// ==========================================================
+// NUEVAS CONSTANTES DE CONEXIÓN (CLEVER CLOUD)
+// ==========================================================
+define('DB_SERVER', 'bj26obu2xeov4c019ku5-mysql.services.clever-cloud.com');
+define('DB_USERNAME', 'ugvczllv19rmup3a');
+define('DB_PASSWORD', 'oK3JLHV6SXl1LJZhPaZgE');
+define('DB_NAME', 'bj26obu2xeov4c019ku5');
+define('DB_PORT', 3306);
+// ==========================================================
+
 ?>
 
