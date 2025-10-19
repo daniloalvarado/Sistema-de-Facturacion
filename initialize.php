750d8fd20132611e3ff50618f76c6697589d5033
Danilo Alvarado <daniloalvarado2002@gmail.com>
2025-10-18 22:39:42 -0500

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -13,7 +13,6 @@
 
 // PARA EL RAILWAY DATABASE CONEXIÓN EN LA NUBE
 // $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-// // if(!defined('base_url')) define('base_url','http://localhost/Sistema de Facturación 2/');
 // if(!defined('base_url')) define('base_url','https://sistema-de-facturacion-8yk6.onrender.com/');
 // if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 // if(!defined('dev_data')) define('dev_data',$dev_data);
@@ -24,24 +23,22 @@
 // define('DB_PORT', 30688); 
 // FINAL PARA EL RAILWAY DATABASE CONEXIÓN EN LA NUBE
 
-
+// PARA EL CLEVER CLOUD CONEXIÓN EN LA NUBE
 // Datos del desarrollador (sin cambios)
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-
 // URL base (sin cambios, apunta a Render)
 if(!defined('base_url')) define('base_url','https://sistema-de-facturacion-8yk6.onrender.com/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
-
 // ==========================================================
 // NUEVAS CONSTANTES DE CONEXIÓN (CLEVER CLOUD)
 // ==========================================================
-define('DB_SERVER', 'bj26obu2xeov4c0l9ku5-mysql.services.clever-cloud.com');
-define('DB_USERNAME', 'ugvczllv19rmup3a');
-define('DB_PASSWORD', 'oK3JLHV6SXlLJZhPaZqE');
-define('DB_NAME', 'bj26obu2xeov4c0l9ku5');
+define('DB_SERVER', 'b0ecjoiz2jpdmx7luq7i-mysql.services.clever-cloud.com');
+define('DB_USERNAME', 'uo8pp47qpjteiacx');
+define('DB_PASSWORD', 'uo8pp47qpjteiacx');
+define('DB_NAME', 'b0ecjoiz2jpdmx7luq7i');
 define('DB_PORT', 3306);
 // ==========================================================
-
+// FINAL PARA EL CLEVER CLOUD CONEXIÓN EN LA NUBE
 ?>
 
